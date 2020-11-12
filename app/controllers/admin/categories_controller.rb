9bd26c2d26ba7e7871628c648b0814a506aa2f10
Will <wdportman@gmail.com>
2020-11-12 12:37:49 -0800

    Finish setting up category controller & views

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CategoriesController < ApplicationController
-  def index
-  end
 
-  def create
+  http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
