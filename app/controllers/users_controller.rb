ef3bb225394feaa6ff4e0109a2e94e00d7733406
Will <wdportman@gmail.com>
2020-11-12 23:10:06 -0800

    Split name field on signup to fname and lname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  params.require(:user).permit(:fname, :lname, :email, :password, :password_confirmation)
 end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,8 @@ ActiveRecord::Schema.define(version: 20201113054926) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "fname"
+    t.string   "lname"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
